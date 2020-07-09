4384701905c6d071f2bd6f282c8f08b44d2b64fc
jhye0n <pwn3rhyun@naver.com>
2020-07-09 10:56:42 +0900

    add makefile

--- sum.cpp
+++ sum.cpp
@@ -1,10 +1,10 @@
-#include <sum.h> /* include_header */
+#include "sum.h" /* include_header */
 
 int sum(int n){
 	/*int i = 1;*/
 	int sum = 0;
 
-	for(int i=1; i<sum; i++){
+	for(int i=1; i<=10; i++){
 		sum += i;
 	}
 
